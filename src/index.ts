4215ef17d0a5e81e7d11ab5b83e875fbdb197cd1
nkwtnb <n.watanabe0305@gmail.com>
2023-09-02 12:15:32 +0900

    add ts-node

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
-const { PDFDocument } = require("pdf-lib")
+const { PDFDocument, rgb } = require("pdf-lib")
+
 const fs = require("fs")
 
-const makePDF = (pdfBytes, fileName) => {
+const makePDF = (pdfBytes: ArrayBuffer, fileName: string) => {
   try {
     fs.writeFileSync('./' + fileName, pdfBytes)
   } catch (err) {
@@ -10,11 +11,34 @@ const makePDF = (pdfBytes, fileName) => {
   }
 }
 
+const to01from256 = (r: number, g: number, b: number) => {
+  if (r < 0 || r > 255) {
+    throw new Error("r is invalid value : " + r)
+  }
+  if (g < 0 || g > 255) {
+    throw new Error("g is invalid value : " + r)
+  }
+  if (b < 0 || b > 255) {
+    throw new Error("b is invalid value : " + r)
+  }
+  return [
+    (r / 255),
+    (g / 255),
+    (b / 255),
+  ]
+}
+
 const create = async () => {
   // PDF Creation
   const pdfDoc = await PDFDocument.create()
   const page = pdfDoc.addPage()
-  page.drawText('You can create PDFs!')
+  const [r, g, b] = to01from256(248,180,0)
+  page.drawText(
+    'You can create PDFs!',
+    {
+      color: rgb(r, g, b),
+    }
+  )
   const pdfBytes = await pdfDoc.save()
   makePDF(pdfBytes, "test.pdf")
 }
@@ -23,7 +47,12 @@ const modify = async () => {
   const uint8Array = fs.readFileSync('./test.pdf')
   const pdfDoc = await PDFDocument.load(uint8Array)
   const pages = pdfDoc.getPages()
-  pages[0].drawText('You can modify PDFs too!')
+  pages[0].drawText(
+    'You can modify PDFs too!',
+    {
+      color: rgb(0, 0.5, 0),
+    }
+  )
   const pdfBytes = await pdfDoc.save()
   makePDF(pdfBytes, "test2.pdf")
 }
